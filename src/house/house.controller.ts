738a42b0c61fafbe3c61d8a5427877c4b5fc6c7e
Axell <axelferchak@gmail.com>
2022-11-20 00:06:08 +0100

    house entity controller service simple

--- src/house/house.controller.ts
+++ src/house/house.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { HouseService } from './house.service';
 
-@Controller('house')
-export class HouseController {}
+@Controller('houses')
+export class HouseController {
+  constructor(private houseService: HouseService) {}
+
+  @Get()
+  findAll() {
+    return this.houseService.getAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.houseService.getHouse(id);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.houseService.createHouse(body);
+  }
+
+  /*     @Put(':id')
+    update(@Param('id') id: number, @Body() body: any) {
+      return body;
+    }
+  
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+      return true;
+    } */
+}
--- src/house/house.service.ts
+++ src/house/house.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { House } from './house.entity';
 
 @Injectable()
-export class HouseService {}
+export class HouseService {
+  constructor(
+    @InjectRepository(House) private houseRepository: Repository<House>,
+  ) {}
+
+  getAll() {
+    this.houseRepository.find();
+  }
+
+  getHouse(id: number) {
+    return this.houseRepository.findOne({ where: { id } });
+  }
+
+  createHouse(body: any) {
+    const newHouse = this.houseRepository.create(body);
+    return this.houseRepository.save(newHouse);
+  }
+}
